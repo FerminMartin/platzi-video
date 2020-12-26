95fe0434b8ef325d3e1eb1d5e45c70134db00875
fermin <fermartorr01@gmail.com>
2020-12-26 17:20:21 +0100

    COMPONENTE: LAYOUT
    -Creo un componente Layout al que le agrego el className="App" + componentes Header y Footer
    -Dentro colocamos un children
    -Evito ducplicados en Home quitando className="App" y Header y Footer
    -paso el div de la home a un Fragment

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -14,10 +12,8 @@ const Home = () => {
     const initialState = useInitialState(API);
     return initialState.length === 0 ? <h1>Loading...</h1> : 
         (
-            <div className="App">
-                <Header />
+            <>
                 <Search />
-
                 {initialState.mylist.length > 0 &&
                 <Categories title="Mi lista">
                     <Carousel>
@@ -41,9 +37,7 @@ const Home = () => {
                         )}
                     </Carousel>
                 </Categories>
-
-                <Footer />
-            </div>
+            </>
         );
 };
 
