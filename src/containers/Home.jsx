69684f2e738eda5021ee74dd09de6f859157b605
fermin <fermartorr01@gmail.com>
2020-12-28 17:23:42 +0100

    VALIDACIONES DE UI
    -importo classNames from classnames (paquete instalado) para agregar validaciones
    -convierto el componente que antes era presentacional a funcional
    -creo la constante que se va a aencargar de manejar los estilos del input inputStyle
    -utilizamos classNames y le vamos a pasar el valor inicial input y el valor que va a validar isHome. Si esto se cumple hará que se presente el valor como una propiedad de css que necesitemos
    -como traemos isHome de los props hay que estructurarlo como la propiedad que tenemos que es isHome
    -una vez hechos los pasos paso el valor a className (dentro del return) para que pueda hacer la validación
    -arreglo temas de estilo: en header agrego el brode redondeado y ajusto la imagen con cover.
    -doy estilos al botón general en app.scss y añado un espacio en login.jsx para separar dos palabras

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,36 +11,36 @@ const Home = ({ myList, trends, originals }) => {
     return (
             <>
             <Header />
-                <Search />
-                {myList.length > 0 &&
-                <Categories title="Mi lista">
-                    <Carousel>
-                        {myList.map(item =>
-                            <CarouselItem 
-                                key={item.id}
-                                {...item} 
-                                isList
-                                />                    
-                        )}
-                    </Carousel>
-                </Categories>            
-                }
+            <Search isHome />
+            {myList.length > 0 &&
+            <Categories title="Mi lista">
+                <Carousel>
+                    {myList.map(item =>
+                        <CarouselItem 
+                            key={item.id}
+                            {...item} 
+                            isList
+                            />                    
+                    )}
+                </Carousel>
+            </Categories>            
+            }
 
-                <Categories title="Tendencias">
-                    <Carousel>
-                        {trends.map(item =>
-                            <CarouselItem key={item.id}{...item} />                    
-                        )}
-                    </Carousel>
-                </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    {trends.map(item =>
+                        <CarouselItem key={item.id}{...item} />                    
+                    )}
+                </Carousel>
+            </Categories>
 
-                <Categories title="Originales de PlatiVideo">
-                    <Carousel>
-                        {originals.map(item =>
-                            <CarouselItem key={item.id}{...item} />                    
-                        )}
-                    </Carousel>
-                </Categories>
+            <Categories title="Originales de PlatiVideo">
+                <Carousel>
+                    {originals.map(item =>
+                        <CarouselItem key={item.id}{...item} />                    
+                    )}
+                </Carousel>
+            </Categories>
             </>
         );
 };
